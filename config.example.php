bceb3ff54a2255cafcda8fe530a326a60f642c53
Pirulug <pirulug@gmail.com>
2024-01-10 00:28:09 -0500

    delete vendor

--- config.example.php
+++ config.example.php
@@ -7,13 +7,19 @@
 
 // Configuración de la base de datos
 const DB_HOST = "localhost";
-const DB_NAME = "inventario";
+const DB_NAME = "pirupos";
 const DB_USER = "root";
 const DB_PASS = "";
 
 // Configuración de la aplicación
-const APP_NAME = "FERTHI";
-const APP_URL = "http://inventorio.test"; // Sin "/" al final de la url
+const APP_NAME = "PiruPOS";
+const APP_URL = "http://pirupos.test"; // Sin "/" al final de la url
+
+// Nombre de la empresa
+const BUSSINES_NAME = "PiruPos E.I.R.L";
+const BUSSINES_LOGO = "/uploads/images/logo_bussines.jpg";
+
+const CORRELATIVE_INIT = "NV01-00000000";
 
 // Claves
 const METHOD = "AES-256-CBC";
@@ -22,3 +28,7 @@ const SECRET_IV = '456232132132432234132';
 
 // Configuración de la zona horaria
 date_default_timezone_set("America/Lima");
+
+// APISPERU
+const APIPERU_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Inhpcm93aTMzNjFAdGFvYnVkYW8uY29tIn0.cvw02v4R0kTaLqG4CdODqWIjb4G5Jac1Q9Vq20akZuc";
+
